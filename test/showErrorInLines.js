00b46c489c79f52d74d8de688cdc381fbeff0300
David McArthur <david.mcarthur.2@glasgow.ac.uk>
2020-01-26 01:07:14 +0000

    add console log in ci

--- test/showErrorInLines.js
+++ test/showErrorInLines.js
@@ -1,130 +1,131 @@
-const test = require('ava')
-const {showErrorInLines} = require('../src/create-code-snippet')
+const test = require("ava");
+const { showErrorInLines } = require("../src/create-code-snippet");
 
-test('show error on first character of first line without color', async (t) => {
+test("show error on first character of first line without color", async t => {
   const snippet = [
     {
       index: 0,
-      content: 'CREATE TABLE foo();'
+      content: "CREATE TABLE foo();"
     },
     {
       index: 1,
-      content: 'CREATE TABLE bar();'
+      content: "CREATE TABLE bar();"
     },
     {
       index: 2,
-      content: 'CREATE TABLE baz();'
+      content: "CREATE TABLE baz();"
     }
-  ]
-  const output = showErrorInLines(snippet, 0, 0, {showColors: false})
+  ];
+  const output = showErrorInLines(snippet, 0, 0, { showColors: false });
   t.deepEqual(output, [
-    { index: 0, content: 'CREATE TABLE foo();' },
-    { index: null, content: '^' },
-    { index: 1, content: 'CREATE TABLE bar();' },
-    { index: 2, content: 'CREATE TABLE baz();' }
-  ])
-})
+    { index: 0, content: "CREATE TABLE foo();" },
+    { index: null, content: "^" },
+    { index: 1, content: "CREATE TABLE bar();" },
+    { index: 2, content: "CREATE TABLE baz();" }
+  ]);
+});
 
-test('show error on last character of last line without color', async (t) => {
+test("show error on last character of last line without color", async t => {
   const snippet = [
     {
       index: 0,
-      content: 'CREATE TABLE foo();'
+      content: "CREATE TABLE foo();"
     },
     {
       index: 1,
-      content: 'CREATE TABLE bar();'
+      content: "CREATE TABLE bar();"
     },
     {
       index: 2,
-      content: 'CREATE TABLE baz();'
+      content: "CREATE TABLE baz();"
     }
-  ]
-  const output = showErrorInLines(snippet, 2, 18, {showColors: false})
+  ];
+  const output = showErrorInLines(snippet, 2, 18, { showColors: false });
   t.deepEqual(output, [
-    { index: 0, content: 'CREATE TABLE foo();' },
-    { index: 1, content: 'CREATE TABLE bar();' },
-    { index: 2, content: 'CREATE TABLE baz();' },
-    { index: null, content: '                  ^' }
-  ])
-})
+    { index: 0, content: "CREATE TABLE foo();" },
+    { index: 1, content: "CREATE TABLE bar();" },
+    { index: 2, content: "CREATE TABLE baz();" },
+    { index: null, content: "                  ^" }
+  ]);
+});
 
-test('show error on first character of first line with color', async (t) => {
+test("show error on first character of first line with color", async t => {
   const snippet = [
     {
       index: 0,
-      content: 'CREATE TABLE foo();'
+      content: "CREATE TABLE foo();"
     },
     {
       index: 1,
-      content: 'CREATE TABLE bar();'
+      content: "CREATE TABLE bar();"
     },
     {
       index: 2,
-      content: 'CREATE TABLE baz();'
+      content: "CREATE TABLE baz();"
     }
-  ]
-  const output = showErrorInLines(snippet, 0, 0)
+  ];
+  const output = showErrorInLines(snippet, 0, 0);
   // console.log(output.map(o => o.content).join('\n'))
-  t.snapshot(output)
-})
+  t.snapshot(output);
+});
 
-test('show error on last character of last line with color', async (t) => {
+test("show error on last character of last line with color", async t => {
   const snippet = [
     {
       index: 0,
-      content: 'CREATE TABLE foo();'
+      content: "CREATE TABLE foo();"
     },
     {
       index: 1,
-      content: 'CREATE TABLE bar();'
+      content: "CREATE TABLE bar();"
     },
     {
       index: 2,
-      content: 'CREATE TABLE baz();'
+      content: "CREATE TABLE baz();"
     }
-  ]
-  const output = showErrorInLines(snippet, 2, 18)
+  ];
+  const output = showErrorInLines(snippet, 2, 18);
   // console.log(output.map(o => o.content).join('\n'))
-  t.snapshot(output)
-})
+  t.snapshot(output);
+});
 
-test('show error on first character of first line with errorStyle highlight', async (t) => {
+test("show error on first character of first line with errorStyle highlight", async t => {
   const snippet = [
     {
       index: 0,
-      content: 'CREATE TABLE foo();'
+      content: "CREATE TABLE foo();"
     },
     {
       index: 1,
-      content: 'CREATE TABLE bar();'
+      content: "CREATE TABLE bar();"
     },
     {
       index: 2,
-      content: 'CREATE TABLE baz();'
+      content: "CREATE TABLE baz();"
     }
-  ]
-  const output = showErrorInLines(snippet, 0, 0, {errorStyle: 'highlight'})
+  ];
+  const output = showErrorInLines(snippet, 0, 0, { errorStyle: "highlight" });
   // console.log(output.map(o => o.content).join('\n'))
-  t.snapshot(output)
-})
+  t.snapshot(output);
+});
 
-test('show error on last character of last line with errorStyle highlight', async (t) => {
+test.only("show error on last character of last line with errorStyle highlight", async t => {
   const snippet = [
     {
       index: 0,
-      content: 'CREATE TABLE foo();'
+      content: "CREATE TABLE foo();"
     },
     {
       index: 1,
-      content: 'CREATE TABLE bar();'
+      content: "CREATE TABLE bar();"
     },
     {
       index: 2,
-      content: 'CREATE TABLE baz();'
+      content: "CREATE TABLE baz();"
     }
-  ]
-  const output = showErrorInLines(snippet, 2, 18, {errorStyle: 'highlight'})
-  // console.log(output.map(o => o.content).join('\n'))
-  t.snapshot(output)
-})
+  ];
+  const output = showErrorInLines(snippet, 2, 18, { errorStyle: "highlight" });
+  console.log(output.map(o => o.content).join("\n"));
+  console.log(output);
+  t.snapshot(output);
+});
